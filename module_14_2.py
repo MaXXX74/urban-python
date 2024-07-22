561a2218da5ac0cd4a283cf890a336b56d5e2c2c
Ragalevich Maxim <91724100+MaXXX74@users.noreply.github.com>
2024-07-22 13:56:26 +0300

    homework 14.2

---
+++ module_14_2.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+import sqlite3
+
+conn = sqlite3.connect("not_telegram.db")
+cursor = conn.cursor()
+
+# удаление из базы данных not_telegram.db запись с id = 6
+cursor.execute("DELETE FROM Users WHERE id = 6")
+conn.commit()
+
+# подсчитываем общее количество записей
+total_users = cursor.execute("SELECT COUNT(*) FROM Users").fetchone()[0]
+
+# рассчитываем сумму всех балансов
+all_balances = cursor.execute("SELECT SUM(balance) FROM Users").fetchone()[0]
+print(all_balances / total_users)
+conn.close()
